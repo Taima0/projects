ea08740b1066e29eb88e8d6939979694ac156141
Taima0 <ttraore@ucsc.edu>
2025-07-11 16:09:48 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const adsData = [
+  { campaign: "Campaign A", date: "2025-07-01", impressions: 1000, clicks: 50, spend: 125 },
+  { campaign: "Campaign A", date: "2025-07-02", impressions: 1200, clicks: 65, spend: 140 },
+  { campaign: "Campaign B", date: "2025-07-01", impressions: 1500, clicks: 80, spend: 200 },
+  { campaign: "Campaign B", date: "2025-07-02", impressions: 1600, clicks: 90, spend: 210 },
+];
